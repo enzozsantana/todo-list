cbb94178f6e185a84eaf2e76813d39211da071c7
enzozsantana <enzozanattadesantana@gmail.com>
2022-09-28 17:16:44 -0300

    first commit

---
+++ src/components/EditTodoDialog.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TextField } from '@mui/material';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function EditTodoDialog({open, dialogHandler, todo, editTodo}) {
+  const [editedText, setEditedText] = React.useState(todo.text);
+
+  const textHandler = () => {
+    editTodo(todo.id, editedText);
+    dialogHandler();
+  }
+
+  return (
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={dialogHandler}
+        aria-describedby="alert-dialog-slide-description"
+        fullWidth
+      >
+        <DialogTitle>{"Editar tarefa"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            <TextField fullWidth defaultValue={todo.text} onChange={(e) => setEditedText(e.target.value)} />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={dialogHandler}>Cancelar</Button>
+          <Button onClick={textHandler}>Ok</Button>
+        </DialogActions>
+      </Dialog>
+  );
+}
